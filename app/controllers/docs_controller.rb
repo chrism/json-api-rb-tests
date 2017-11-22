c4c7e427f3abd541e4cdc88db52d7e888e4d0c2f
Chris Masters <chrismasters@gmail.com>
2017-11-22 16:40:29 +0100

    Install from template

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,21 @@
+require 'jsonapi_swagger_helpers'
+
+class DocsController < ActionController::API
+  include JsonapiSwaggerHelpers::DocsControllerMixin
+
+  swagger_root do
+    key :swagger, '2.0'
+    info do
+      key :version, '1.0.0'
+      key :title, 'JSONAPI RB TESTS'
+      key :description, 'Testing the jsonapi-rb project'
+      contact do
+        key :name, 'Chris Masters'
+        key :email, 'chrismasters@gmail.com'
+      end
+    end
+    key :basePath, '/api'
+    key :consumes, ['application/json']
+    key :produces, ['application/json']
+  end
+end
